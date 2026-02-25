ff2c02c9fe85eed30f801f696a317791137ef283
shivankupadhyay-eng <shivankupadhyay51@gmail.com>
2026-02-25 16:46:59 +0530

    train data with the booking data

--- Booking/serializers.py
+++ Booking/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import BookingData
+from Train.serializers import TrainDataListSerializer
 
 class BookingDataSerializer(serializers.ModelSerializer):
     user_email = serializers.EmailField(source='user.email', read_only=True)
@@ -9,7 +10,7 @@ class BookingDataSerializer(serializers.ModelSerializer):
         fields = ['id', 'user_email', 'train_number', 'booking_date', 'seat_requested']
 
 class BookingListSerializer(serializers.ModelSerializer):
-    train_number = serializers.CharField(source='train.train_number', read_only=True)
+    train = TrainDataListSerializer(read_only=True)
     class Meta:
         model = BookingData
-        fields = ['train_number', 'booking_date', 'seat_requested']
+        fields = ['train', 'booking_date', 'seat_requested']
